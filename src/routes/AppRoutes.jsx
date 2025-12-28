9e6cd61177bbeb8f315009f344a2aea4d6dd9961
AdkNiruta1 <adkniru184@gmail.com>
2025-12-28 21:58:37 +0545

    remove the header in each files

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -14,10 +14,13 @@ import ForgotPassword from "../pages/auth/login/pages/ForgetPassword";
 import Register from "../pages/auth/Register/pages/Register";
 import AdminVerifyDocuments from "../pages/admin/users/pages/AdminVeriedDoc";
 import AdminVerifiedUsers from "../pages/admin/users/pages/AdminVerifiedUsers";
+import { Header } from "../components/Header";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
+      {/* Header always visible */}
+      <Header />
       <Routes>
         {/* Public Routes */}
         <Route path="/" element={<Index />} />
@@ -28,21 +31,24 @@ export default function AppRoutes() {
         <Route path="/email-verification" element={<VerifyEmail />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/browse" element={<BrowseFood />} />
-        {/* Role-based Protected */}
+
+        {/* Admin */}
         <Route element={<ProtectedRoute roles={["admin"]} />}>
           <Route path="/admin" element={<AdminDashboard />} />
           <Route path="/manage-users" element={<AdminVerifiedUsers />} />
           <Route path="/admin/verify-documents" element={<AdminVerifyDocuments />} />
-
-
-
         </Route>
+
+        {/* Recipient */}
         <Route element={<ProtectedRoute roles={["recipient"]} />}>
           <Route path="/recipient-dashboard" element={<RecipientDashboard />} />
         </Route>
+
+        {/* Donor */}
         <Route element={<ProtectedRoute roles={["donor"]} />}>
           <Route path="/donor-dashboard" element={<DonorDashboard />} />
         </Route>
+
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
