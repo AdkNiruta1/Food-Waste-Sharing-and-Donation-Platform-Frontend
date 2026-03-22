8ddf6976c439adab7b07ed01be842dcbdd16398f
Niruta Adhikari <adkniru184@gmail.com>
2026-03-23 01:23:19 +0545

    fix bug

--- src/pages/donor/dashboard/pages/ViewFoodRequestLIst.jsx
+++ src/pages/donor/dashboard/pages/ViewFoodRequestLIst.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { Card, CardContent, CardHeader, CardTitle } from "../../../../components/ui/card";
 import { Button } from "../../../../components/ui/button";
-import { Separator } from "../../../../components/ui/separator";
 import { Badge } from "../../../../components/ui/badge";
 import {
   ArrowLeft,
