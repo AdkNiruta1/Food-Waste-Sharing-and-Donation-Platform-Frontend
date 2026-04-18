509923904656ffedbc3cc568071bc09663962785
AdkNiruta1 <adkniru184@gmail.com>
2026-04-18 19:40:34 +0545

    update url

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,8 +1,8 @@
 // Base URLs for API and images
 // local api or url of backend
-export const BASE_URL = 'http://localhost:5000/api';
-export const IMAGE_URL = 'http://localhost:5000/';
+// export const BASE_URL = 'http://localhost:5000/api';
+// export const IMAGE_URL = 'http://localhost:5000/';
 
 // server api or url of backend
-   // export const BASE_URL = 'https://annapurna-bhandar.onrender.com/api';
-   // export const IMAGE_URL = 'https://annapurna-bhandar.onrender.com/';
+   export const BASE_URL = 'https://annapurna-bhandar.onrender.com/api';
+   export const IMAGE_URL = 'https://annapurna-bhandar.onrender.com/';
